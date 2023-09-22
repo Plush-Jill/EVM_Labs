e23ed9786e4bdfed5eaf9acbef8fc9d76d2928dc
Plush-Jill <bockarevvlad23@gmail.com>
2023-09-22 11:13:15 +0700

    x = 10;

--- Lab_1/main.cpp
+++ Lab_1/main.cpp
@@ -6,9 +6,9 @@ int main(){
 
     size_t N = 1'000'000'000;
     double ln = 0;
-
+    double x = 10;
     for (size_t n {1}; n < N; ++n){
-        ln += (pow(-1, (double)(!(n % 2)))) * pow(10, (double)n) / (double)n;
+        ln += (pow(-1, (double)(!(n % 2)))) * pow(x, (double)n) / (double)n;
     }
     //printf("%e", ln);
 
