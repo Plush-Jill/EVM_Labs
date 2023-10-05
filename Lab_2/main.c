613ab3f7a136e82832ec6d789211de1b157cd44e
Plush-Jill <bockarevvlad23@gmail.com>
2023-10-06 00:29:56 +0700

    Написал алгоритм и скрипт для множественного запуска и измерения времени, на измерение времени уйдёт почти 2 часа.

--- Lab_2/main.c
+++ Lab_2/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
+int main(int argc, char** argv){
 
-int main(){
-
-    size_t N = 1'000'000'000;
+    size_t N = (size_t)atoll(argv[1]);
     double ln = 0;
     double x = 10;
+
     for (size_t n = 0; n < N; ++n){
-        ln += (pow(-1, (double)(!(n % 2)))) * pow(x, (double)n) / (double)n;
+        ln += (pow(-1, (double)(!(n % 2)))) * pow(x, (double)(n-1));
     }
-    printf("%e", ln);
 
 
     return 0;
