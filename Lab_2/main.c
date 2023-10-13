bfe8ce71470e922b9e3ec03cea3d1d9b218d73ed
Plush-Jill <bockarevvlad23@gmail.com>
2023-10-13 11:01:26 +0700

    a

--- Lab_2/main.c
+++ Lab_2/main.c
@@ -7,9 +7,14 @@ int main(int argc, char** argv){
     size_t N = (size_t)atoll(argv[1]);
     double ln = 0;
     double x = 10;
+    double pow1;
+    double pow2;
+
 
     for (size_t n = 0; n < N; ++n){
-        ln += (pow(-1, (double)(!(n % 2)))) * pow(x, (double)(n)) / (double)n;
+        pow1 = pow(-1, (double)(!(n%2)));
+        pow2 = pow(x, (double)n);
+        ln += pow1 * pow2 / (double)n;
     }
 
 
