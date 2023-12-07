71d78aa59ba831558187431d29993f63cb473731
Plush-Jill <bockarevvlad23@gmail.com>
2023-12-07 22:51:18 +0700

    Всё готово.

--- Lab_9/main.cpp
+++ Lab_9/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <exception>
 #include <fstream>
+#include <climits>
+
 
 
 /// __builtin_ia32_rdtsc()
-///    printf("N = %lld, Tick = %llu\n", N*4, (end-start) / N / K);
-
 #define KB (int)1024
 #define MB (KB * 1024)
 #define L1CacheSize (48 * KB)
@@ -14,55 +14,56 @@
 #define intArraySizeForL1CacheSize (L1CacheSize / (int)sizeof(int))
 #define intArraySizeForKB ((int)1024 / (int)(sizeof(int)))
 #define intArraySizeForMB (MB / (int)(sizeof(int)))
-#define SizesCount (int)33
-#define bypassCountPerSize (int)500
+#define bypassCount (int)(1000 * 50000)
+#define fragmentsOffset intArraySizeForL1CacheSize
+
 
 
 int main(){
 
     std::ofstream resultsOutput("Lab_9_bypass_results.txt", std::ios::out);
+    std::ofstream trashOutput("trash_output.txt", std::ios::out);
+
     if (!resultsOutput.is_open()){
         throw std::exception();
     }
 
+    int *array = new int[32 * intArraySizeForL1CacheSize];
+
     for (int fragmentsCount {1}; fragmentsCount < 33; ++fragmentsCount){
-        int *array = new int[fragmentsCount * intArraySizeForL1CacheSize];
         int fragmentSize = intArraySizeForL1CacheSize / fragmentsCount;
+
 
         for (int i0{}; i0 < fragmentSize; ++i0){
             int k {i0};
             for (int j{1}; j < fragmentsCount; ++j){
-                array[k] = k + intArraySizeForL1CacheSize;
-                k += intArraySizeForL1CacheSize;
-                if ((k % (intArraySizeForL1CacheSize * fragmentsCount)) < k){
+                array[k] = k + fragmentsOffset;
+                k += fragmentsOffset;
+                if ((k % (fragmentsOffset * fragmentsCount)) < k){
                     throw std::exception();
                 }
-                //k %= (intArraySizeForL1CacheSize * fragmentsCount);
             }
             array[k] = (i0 + 1) % fragmentSize;
         }
 
-        int bypassCountPerSizeForCurrentIteration = bypassCountPerSize * (fragmentsCount < 10 ? fragmentsCount < 5 ? 50 : 5 : 1);
-        if (fragmentsCount < 4){
-            bypassCountPerSizeForCurrentIteration *= 10;
-        }
 
 
         int temp {0};
-
         unsigned long long begin = __builtin_ia32_rdtsc();
-        for (int i {}; i < bypassCountPerSizeForCurrentIteration; ++i){
+        for (int i {}; i < bypassCount; ++i){
             temp = array[temp];
         }
         unsigned long long end = __builtin_ia32_rdtsc();
-        std::cout << temp;
+        trashOutput << temp;
 
         std::string fragmentsCountInString = std::to_string(fragmentsCount) + " fragments";
-        resultsOutput << fragmentsCountInString << ';' << (end - begin) / L1CacheSize / bypassCountPerSizeForCurrentIteration << std::endl;
-
+        resultsOutput << fragmentsCountInString << ';' << (end - begin) / bypassCount << std::endl;
+        std::cout << fragmentsCountInString << ';' << (end - begin) / bypassCount << std::endl;
 
     }
 
 
+    free(array);
+    resultsOutput.close();
     return 0;
 }
