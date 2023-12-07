04ad9f5560e6fb85232e682799c3babe1d6345d2
Plush-Jill <bockarevvlad23@gmail.com>
2023-12-07 23:30:33 +0700

    Добавил размеров и сдвигов.

--- Lab_9/main.cpp
+++ Lab_9/main.cpp
@@ -12,11 +12,14 @@
 #define L2CacheSize ((1024 + 256) * KB)
 #define L3CacheSize (12 * MB)
 #define intArraySizeForL1CacheSize (L1CacheSize / (int)sizeof(int))
+#define intArraySizeForL3CacheSize (L3CacheSize / (int)sizeof(int))
 #define intArraySizeForKB ((int)1024 / (int)(sizeof(int)))
 #define intArraySizeForMB (MB / (int)(sizeof(int)))
-#define bypassCount (int)(1000 * 50000)
-#define fragmentsOffset intArraySizeForL1CacheSize
-
+#define bypassCount (int)(1000 * 80000)
+#define fragmentsOffsetL1 intArraySizeForL1CacheSize
+#define fragmentsOffsetL3 intArraySizeForL3CacheSize
+#define intArrayUniversalSize (1024 * 1024 * 16 / sizeof(int))
+#define fragmentOffsetUniversal (1024 * 1024 * 16 / sizeof(int))
 
 
 int main(){
@@ -37,9 +40,9 @@ int main(){
         for (int i0{}; i0 < fragmentSize; ++i0){
             int k {i0};
             for (int j{1}; j < fragmentsCount; ++j){
-                array[k] = k + fragmentsOffset;
-                k += fragmentsOffset;
-                if ((k % (fragmentsOffset * fragmentsCount)) < k){
+                array[k] = k + fragmentsOffsetL1;
+                k += fragmentsOffsetL1;
+                if ((k % (fragmentsOffsetL1 * fragmentsCount)) < k){
                     throw std::exception();
                 }
             }
@@ -58,7 +61,7 @@ int main(){
 
         std::string fragmentsCountInString = std::to_string(fragmentsCount);// + " fragments";
         resultsOutput << fragmentsCountInString << ';' << (end - begin) / bypassCount << std::endl;
-        //std::cout << fragmentsCountInString << ';' << (end - begin) / bypassCount << std::endl;
+        std::cout << fragmentsCountInString << ';' << (end - begin) / bypassCount << std::endl;
 
     }
 
